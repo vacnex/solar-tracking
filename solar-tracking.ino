f1c9547913d9eeb2655fd1fed39fb17b73816b83
VAC <37587399+vacnex@users.noreply.github.com>
2020-07-14 23:27:10 +0700

    Full code for Arduino

---
+++ solar-tracking.ino
@@ -0,0 +1,110 @@
+#include <Servo.h> 
+
+
+Servo horizontal; 
+int servoh = 180;   
+
+int servohLimitHigh = 180;
+int servohLimitLow = 15;
+
+Servo vertical;
+int servov = 45;
+
+int servovLimitHigh = 45;
+int servovLimitLow = 0;
+
+int ldrlt = 0; //LDR top left - BOTTOM LEFT    <--- BDG
+int ldrrt = 1; //LDR top rigt - BOTTOM RIGHT 
+int ldrld = 2; //LDR down left - TOP LEFT
+int ldrrd = 3; //ldr down rigt - TOP RIGHT
+
+void setup()
+{
+  Serial.begin(9600);
+  horizontal.attach(9); 
+  vertical.attach(10);
+  horizontal.write(100);
+  vertical.write(45);
+  delay(1000);
+}
+
+void loop() 
+{
+  int lt = analogRead(ldrlt); // top left
+  int rt = analogRead(ldrrt); // top right
+  int ld = analogRead(ldrld); // down left
+  int rd = analogRead(ldrrd); // down rigt
+  
+  int dtime = 10;
+  int tol = 50;
+  
+  int avt = (lt + rt) / 2; // average value top
+  int avd = (ld + rd) / 2; // average value down
+  int avl = (lt + ld) / 2; // average value left
+  int avr = (rt + rd) / 2; // average value right
+
+  int dvert = avt - avd; // check the diffirence of up and down
+  int dhoriz = avl - avr;// check the diffirence og left and rigt
+  
+  
+  Serial.print(avt);
+  Serial.print(" ");
+  Serial.print(avd);
+  Serial.print(" ");
+  Serial.print(avl);
+  Serial.print(" ");
+  Serial.print(avr);
+  Serial.print("   ");
+  Serial.print(dtime);
+  Serial.print("   ");
+  Serial.print(tol);
+  Serial.println(" ");
+  
+    
+  if (-1*tol > dvert || dvert > tol) // check if the diffirence is in the tolerance else change vertical angle
+  {
+  if (avt > avd)
+  {
+    servov = ++servov;
+     if (servov > servovLimitHigh) 
+     { 
+      servov = servovLimitHigh;
+     }
+  }
+  else if (avt < avd)
+  {
+    servov= --servov;
+    if (servov < servovLimitLow)
+  {
+    servov = servovLimitLow;
+  }
+  }
+  vertical.write(servov);
+  }
+  
+  if (-1*tol > dhoriz || dhoriz > tol) // check if the diffirence is in the tolerance else change horizontal angle
+  {
+  if (avl < avr)
+  {
+    servoh = --servoh;
+    if (servoh < servohLimitLow)
+    {
+    servoh = servohLimitLow;
+    }
+  }
+  else if (avl > avr)
+  {
+    servoh = ++servoh;
+     if (servoh > servohLimitHigh)
+     {
+     servoh = servohLimitHigh;
+     }
+  }
+  else if (avl = avr)
+  {
+  }
+  horizontal.write(servoh);
+  }
+   delay(dtime);
+
+}
